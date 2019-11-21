9889876931c8412f7251f0a75e58939d0906a2b4
Lucas Eriksson <lucas.eriksson@saabgroup.com>
2019-11-21 13:36:49 +0100

    board: Shorten piece placement code

--- board.cpp
+++ board.cpp
@@ -5,7 +5,7 @@
 
 void Board::selectPiece() {
     auto p = static_cast<Tile*>(sender())->getPos();
-    std::cout << "CLICK " << p.first << p.second << std::endl;
+    std::cout << "CLICK " << static_cast<char>(p.second + 65) << (p.first - 8) * -1 << std::endl;
 }
 
 Board::Board(QLayout* l, QWidget *parent, Qt::WindowFlags f)
@@ -18,35 +18,17 @@ Board::Board(QLayout* l, QWidget *parent, Qt::WindowFlags f)
         for (int j = 0; j < 8; ++j) {
             board[i].push_back(new Tile(this, i, j, white = !white));
             static_cast<QGridLayout*>(l)->addWidget(board[i][j], i, j);
-            if(!static_cast<bool>(connect(board[i][j], &Tile::leftClicked, this, &Board::selectPiece))) {
-                std::cout << "Connection failed!" << std::endl;
-            }
+            connect(board[i][j], &Tile::leftClicked, this, &Board::selectPiece);
         }
         white = !white;
     }
 
-    // Black pieces
-    board[0][0]->setPiece(false, 'r');
-    board[0][1]->setPiece(false, 'h');
-    board[0][2]->setPiece(false, 'b');
-    board[0][3]->setPiece(false, 'q');
-    board[0][4]->setPiece(false, 'k');
-    board[0][5]->setPiece(false, 'b');
-    board[0][6]->setPiece(false, 'h');
-    board[0][7]->setPiece(false, 'r');
-    for (auto &q: board[1]) {
-        q->setPiece(false, 'p');
+    // Place pieces
+    QVector<char> pieces{'r','h','b','q','k','b','h','r'};
+    for (int i = 0; i < 8; ++i) {
+        board[0][i]->setPiece(false, pieces[i]);
+        board[1][i]->setPiece(false, 'p');
+        board[6][i]->setPiece(true, 'p');
+        board[7][i]->setPiece(true, pieces[i]);
     }
-    // White pieces
-    for (auto &q: board[6]) {
-        q->setPiece(true, 'p');
-    }
-    board[7][0]->setPiece(true, 'r');
-    board[7][1]->setPiece(true, 'h');
-    board[7][2]->setPiece(true, 'b');
-    board[7][3]->setPiece(true, 'q');
-    board[7][4]->setPiece(true, 'k');
-    board[7][5]->setPiece(true, 'b');
-    board[7][6]->setPiece(true, 'h');
-    board[7][7]->setPiece(true, 'r');
 }
